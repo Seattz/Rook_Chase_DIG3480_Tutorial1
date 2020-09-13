13412a2754e1950d3133c54b976da42738ee1963
BirdiesFrijoles <dufflad007@yahoo.com>
2020-09-13 15:25:13 -0500

    submission on 13/09/2020 15:25:13

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour, RollABallControls.IPlayerActions
+{
+    private float speed = 20; 
+    public Text countText;
+    public Text winText;
+    public RollABallControls controls;
+    public Vector2 motion;
+
+    private Rigidbody rb;
+    private int count;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText ();
+        winText.text = "";
+    }
+
+    public void OnEnable() {
+        if (controls == null) 
+        {
+            controls = new RollABallControls();
+
+            controls.Player.SetCallbacks(this);
+        }
+        controls.Player.Enable();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+       motion = context.ReadValue<Vector2>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(motion.x, 0.0f, motion.y);
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive (false);
+            count = count + 1;
+            SetCountText ();
+        }
+    }
+
+    void SetCountText ()
+    {
+        countText.text = "Count: " + count.ToString ();
+        if (count >= 11)
+        {
+            winText.text = "You Win! Game created by Chase Rook.";
+        }
+    }
+
+}
